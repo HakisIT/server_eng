abd7b3c25696e8b79803284c8e4e154a226d54b1
HakisIT <play.yaroslav@gmail.com>
2024-03-03 16:25:17 +0200

    adding input validation and locking data as a number or empty string

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from flask import Flask, Response, request, json, redirect, url_for, render_template
+from flask import Flask, Response, request, json, redirect, url_for, render_template, jsonify
 from flask_restful import Api, Resource
 import mysql.connector
 from flask_cors import CORS
@@ -41,12 +41,27 @@ def selection_word():
         wrd.append(i[0][:-1])
     return wrd
 
+def validate(data):
+    # check is digit in data
+    if any(char.isdigit() for char in data['english']) or any(char.isdigit() for char in data['russian']) or any(char.isdigit() for char in data['czech']):
+        print('number validate')
+        return False
+
+    if data['english'] == '' or data['russian'] == '' or data['czech'] == '':
+        print('empty validate')
+        return False
+
+    return True
+
 def add_new_word(rcv_data):
-    sql_insert_query = """INSERT INTO main (english, russian, chech) 
-                                VALUES (%s, %s, %s)"""
-    tuple1 = (rcv_data['english'], rcv_data['russian'], rcv_data['czech'])
-    mycursor.execute(sql_insert_query, tuple1)
-    if mydb.commit():
+    print('rcv_data', rcv_data)
+
+    if validate(rcv_data) == True:
+        sql_insert_query = """INSERT INTO main (english, russian, chech) 
+                                    VALUES (%s, %s, %s)"""
+        tuple1 = (rcv_data['english'], rcv_data['russian'], rcv_data['czech'])
+        mycursor.execute(sql_insert_query, tuple1)
+        mydb.commit()
         return True
     else:
         return False
@@ -69,23 +84,24 @@ def get(word_id):
     return selection_id(word_id)
 
 
-@app.route('/add', methods=['POST', 'GET'])
+@app.route('/add', methods=['POST'])
 def process_data():
-    
     if request.method=='POST':
         if request.data:
             rcv_data = json.loads(request.data.decode(encoding='utf-8'))
-
-            data_success = {'data': {'success': 'true'}}
-            data_fail = {'data': {'success': 'false'}}
+            print('POST rcv_data', rcv_data)
+            data_success = {'data': 'success'}
+            data_fail = {'data': {'success': 'fail'}}
 
             if add_new_word(rcv_data) == True:
-                return data_success
+                print("true", data_success)
+                return jsonify(data_success)
             else:
-                return data_fail
+                print("false", data_fail)
+                return jsonify(data_fail)
             
-    else:
-        return render_template('index.html')
+    # else:
+    #     return render_template('index.html')
 
 @app.route('/<usr>')
 def user(usr):
