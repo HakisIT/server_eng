8079625e7dec0f1751b48f2dc8d6a188ec5badb1
HakisIT <play.yaroslav@gmail.com>
2024-03-10 20:08:53 +0200

    created a new html file for registration, partially made the system of password conversion to hash

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ import mysql.connector
 from flask_cors import CORS
 import random
 import json
+import hashlib
 
 HOST = '192.168.1.35'
 PORT = 9999
@@ -24,13 +25,13 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 
-
 def selection_id(word_id):
     sql_select_query = """SELECT * FROM main WHERE id = %s"""
     mycursor.execute(sql_select_query, (word_id,))
     word1 = mycursor.fetchall()
     dct = {word1[0][0]:[word1[0][1], word1[0][2], word1[0][3]]}
     return Response(str(dct), content_type='text/html; charset=utf-8')
+
 
 def select_main():
     mycursor = mydb.cursor()
@@ -41,6 +42,7 @@ def select_main():
         id_lst.append(i[0])
     return id_lst
 
+
 def selection_word():
     sql_select_query = "SELECT english FROM main"
     mycursor.execute(sql_select_query)
@@ -50,7 +52,8 @@ def selection_word():
         wrd.append(i[0][:-1])
     return wrd
 
-def validate(data):
+
+def validate_word(data):
     # check is digit in data
     if any(char.isdigit() for char in data['english']) or any(char.isdigit() for char in data['russian']) or any(char.isdigit() for char in data['czech']):
         print('number validate')
@@ -59,13 +62,13 @@ def validate(data):
     if data['english'] == '' or data['russian'] == '' or data['czech'] == '':
         print('empty validate')
         return False
-
     return True
+
 
 def add_new_word(rcv_data):
     print('rcv_data', rcv_data)
 
-    if validate(rcv_data) == True:
+    if validate_word(rcv_data) == True:
         sql_insert_query = """INSERT INTO main (english, russian, chech) 
                                     VALUES (%s, %s, %s)"""
         tuple1 = (rcv_data['english'], rcv_data['russian'], rcv_data['czech'])
@@ -76,6 +79,37 @@ def add_new_word(rcv_data):
         return False
     
 
+def validate_regist(data):
+    if data['user'] == '' or data['password'] == '' or data['email'] == '':
+        print('empty validate')
+        return False
+    return True
+
+
+def hash_password(data):
+    if validate_regist(data) == True:
+        new = data.split()
+        hs = hashlib.md5(new.encode()).hexdigest()
+        return hs
+    else:
+        return False
+
+
+def add_new_user(rcv_data):
+    print('rcv_data', rcv_data)
+
+    if hash_password(rcv_data) != '':
+        hash_pass = hash_password(rcv_data['password'])
+        sql_insert_query = """INSERT INTO user_info (user_name, password, email) 
+                                    VALUES (%s, %s, %s)"""
+        tuple1 = (rcv_data['user'], hash_pass, rcv_data['email'])
+        mycursor.execute(sql_insert_query, tuple1)
+        mydb.commit()
+        return True
+    else:
+        return False
+
+    
 # class Main(Resource):
 #     def get(self, word_id):
 #         return selection_id(word_id)
@@ -109,6 +143,22 @@ def process_data():
                 print("false", data_fail)
                 return jsonify(data_fail)
             
+
+@app.route('/registration', methods=['POST'])
+def regist():
+    if request.method=='POST':
+        if request.data:
+            rcv_data = json.loads(request.data.decode(encoding='utf-8'))
+            print('POST rcv_data', rcv_data)
+            data_success = {'data': 'success'}
+            data_fail = {'data': {'success': 'fail'}}
+
+            if add_new_user(rcv_data) == True:
+                print("true", data_success)
+                return jsonify(data_success)
+            else:
+                print("false", data_fail)
+                return jsonify(data_fail)
 
 @app.route('/<usr>')
 def user(usr):
